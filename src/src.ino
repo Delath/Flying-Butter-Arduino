1425136587059d4e7b64a42ec4caf675ae3d46d6
Delath <simoni.dario1@gmail.com>
2022-06-14 20:43:35 +0200

    Testing button

--- src/src.ino
+++ src/src.ino
@@ -1,80 +1,137 @@
 #include <FastLED.h> 
-const int pinLed = 12;
+const int pinLed = 4; // or 7
 const int numLed = 50;
 CRGB leds[numLed];
 
-int val=0;
-int state=0;
-//int motion=0;
+int CLK = 11;
+int DT = 12;
+int RotPosition = 1;
+int rotation;  
+int value;
+boolean LeftRight;
 
-int figurePassed1=0;
-int figurePassed2=0;
-int figurePassed3=0;
-int figurePassed4=0;
+//int val=0;
+//int state=0;
+//int motionDetected=0;
+
+//int figurePassed1=0;
+//int figurePassed2=0;
+//int figurePassed3=0;
+//int figurePassed4=0;
 
 void setup() {
     Serial.begin(115200);
     FastLED.addLeds<WS2812B, pinLed, GRB>(leds, numLed);
 
-    pinMode(10, INPUT_PULLUP);
+    pinMode (CLK,INPUT);
+    pinMode (DT,INPUT);
+    rotation = digitalRead(CLK);  
+
+    //pinMode(10, INPUT_PULLUP);
 }
 
 void loop() {
-    val = digitalRead(10);
-    if(val==HIGH){
-        delay(500);
-        if(state==LOW){
-            //motion=0;
-            state = HIGH;
+    value = digitalRead(CLK);
+    if (value != rotation){ // we use the DT pin to find out which way we turning.
+        if (digitalRead(DT) != value) {  // Clockwise
+            RotPosition ++;
+            LeftRight = true;
+        } else { //Counterclockwise
+            LeftRight = false;
+            if(RotPosition>0){
+                RotPosition--;
+            }
         }
+    }
+    if(RotPosition==0){
+        
+    }else if(RotPosition>=1 && RotPosition<5){
+        lightGreen();
+    }else if(RotPosition>=5 && RotPosition<9){
+        lightYellow();
     }else{
-        delay(500);
-        if(state==HIGH){
-            //motion=1;
-            lightLeds1();
-            figurePassed1++;
-            state=LOW;
-        }
+        lightRed();
+    }
+    //val = digitalRead(10);
+    //if(val==HIGH){
+    //    delay(500);
+    //    if(state==LOW){
+    //        //motionDetected=0;
+    //        state = HIGH;
+    //    }
+    //}else{
+    //    delay(500);
+    //    if(state==HIGH){
+    //        //motionDetected=1;
+    //        lightLeds1();
+    //        figurePassed1++;
+    //        state=LOW;
+    //    }
+    //}
+}
+
+void lightGreen(){
+    delay(40);
+    for (int i = 0 ; i < 50 ; i++) {
+        leds[i] = CRGB(0, 255, 0);
+        FastLED.show();
+        delay(40);
+    }
+}
+void lightYellow(){
+    delay(40);
+    for (int i = 0 ; i < 50 ; i++) {
+        leds[i] = CRGB(255, 255, 0);
+        FastLED.show();
+        delay(40);
+    }
+}
+void lightRed(){
+    delay(40);
+    for (int i = 0 ; i < 50 ; i++) {
+        leds[i] = CRGB(255, 0, 0);
+        FastLED.show();
+        delay(40);
     }
 }
 
-void lightLeds1(){
-    if(figurePassed1<4){
-        delay(40);
-        for (int i = 0 ; i < 3 ; i++) {
-            leds[i+(figurePassed1*3)] = CRGB(255, 0, 0);
-            FastLED.show();
-            delay(40);
-        }
-    }
-}
-void lightLeds2(){
-    if(figurePassed2<4){
-        delay(40);
-        for (int i = 12 ; i < 15 ; i++) {
-            leds[i+(figurePassed2*3)] = CRGB(0, 255, 0);
-            FastLED.show();
-            delay(40);
-        }
-    }
-}
-void lightLeds3(){
-    if(figurePassed3<4){
-        delay(40);
-        for (int i = 24 ; i < 27 ; i++) {
-            leds[i+(figurePassed3*3)] = CRGB(0, 0, 255);
-            FastLED.show();
-            delay(40);
-        }
-    }
-}
-void lightLeds4(){
-    if(figurePassed4<4){
-        delay(40);
-        for (int i = 36 ; i < 39 ; i++) {
-            leds[i+(figurePassed4*3)] = CRGB(255, 255, 0);
-            FastLED.show();
-            delay(40);
-        }
-    }
-}
+//void lightLeds1(){
+//    if(figurePassed1<4){
+//        delay(40);
+//        for (int i = 1 ; i < 4 ; i++) {
+//            leds[i+(figurePassed1*3)] = CRGB(255, 0, 0);
+//            FastLED.show();
+//            delay(40);
+//        }
+//    }
+//}
+//void lightLeds2(){
+//    if(figurePassed2<4){
+//        delay(40);
+//        for (int i = 13 ; i < 16 ; i++) {
+//            leds[i+(figurePassed2*3)] = CRGB(0, 255, 0);
+//            FastLED.show();
+//            delay(40);
+//        }
+//    }
+//}
+//void lightLeds3(){
+//    if(figurePassed3<4){
+//        delay(40);
+//        for (int i = 25 ; i < 28 ; i++) {
+//            leds[i+(figurePassed3*3)] = CRGB(0, 0, 255);
+//            FastLED.show();
+//            delay(40);
+//        }
+//    }
+//}
+//void lightLeds4(){
+//    if(figurePassed4<4){
+//        delay(40);
+//        for (int i = 37 ; i < 40 ; i++) {
+//            leds[i+(figurePassed4*3)] = CRGB(255, 255, 0);
+//            FastLED.show();
+//            delay(40);
+//        }
+//    }
+//}
