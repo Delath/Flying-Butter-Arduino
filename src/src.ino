62bf37daec35094a2f449163662260774fa39d73
degazaki <107436443+degazaki@users.noreply.github.com>
2022-06-14 20:58:54 +0200

    fix

--- src/src.ino
+++ src/src.ino
@@ -145,31 +145,3 @@ void lightRed(){
 //        }
 //    }
 //}
-
-
-
-void loop() { 
-    value = digitalRead(CLK);
-    if (value != rotation){ // we use the DT pin to find out which way we turning.
-        if (digitalRead(DT) != value) {  // Clockwise
-            RotPosition ++;
-            LeftRight = true;
-        } else { //Counterclockwise
-            LeftRight = false;
-            RotPosition--;
-        }
-        if (LeftRight){ // turning right will turn on red led.
-            Serial.println ("clockwise");
-            digitalWrite (RedLed, HIGH);
-            digitalWrite (GreenLed, LOW);
-        }else{        // turning left will turn on green led.   
-            Serial.println("counterclockwise");
-            digitalWrite (RedLed, LOW);
-            digitalWrite (GreenLed, HIGH);
-        }
-        Serial.print("Encoder RotPosition: ");
-        Serial.println(RotPosition);
-    // this will print in the serial monitor.
-    } 
-    rotation = value;
-} 
