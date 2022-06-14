dbccb69d3b1b4ab7cb1b9742dde628331755e628
Delath <simoni.dario1@gmail.com>
2022-06-14 20:55:35 +0200

    minor fix

--- src/src.ino
+++ src/src.ino
@@ -42,21 +42,24 @@ void loop() {
                 RotPosition--;
             }
         }
-    }
-    if (LeftRight){// turning right will turn on red led.
-        Serial.println ("clockwise");
-    }else{// turning left will turn on green led.   
-        Serial.println("counterclockwise");
-    }
 
-    if(RotPosition==0){
+        if (LeftRight){// turning right will turn on red led.
+            Serial.println ("clockwise");
+        }else{// turning left will turn on green led.   
+            Serial.println("counterclockwise");
+        }
 
-    }else if(RotPosition>=1 && RotPosition<5){
-        lightGreen();
-    }else if(RotPosition>=5 && RotPosition<9){
-        lightYellow();
-    }else{
-        lightRed();
+        if(RotPosition==0){
+            
+        }else if(RotPosition>=1 && RotPosition<5){
+            lightGreen();
+        }else if(RotPosition>=5 && RotPosition<9){
+            lightYellow();
+        }else{
+            lightRed();
+        }
+        
+
     }
     rotation = value;
     //val = digitalRead(10);
@@ -142,3 +145,31 @@ void lightRed(){
 //        }
 //    }
 //}
+
+
+
+void loop() { 
+    value = digitalRead(CLK);
+    if (value != rotation){ // we use the DT pin to find out which way we turning.
+        if (digitalRead(DT) != value) {  // Clockwise
+            RotPosition ++;
+            LeftRight = true;
+        } else { //Counterclockwise
+            LeftRight = false;
+            RotPosition--;
+        }
+        if (LeftRight){ // turning right will turn on red led.
+            Serial.println ("clockwise");
+            digitalWrite (RedLed, HIGH);
+            digitalWrite (GreenLed, LOW);
+        }else{        // turning left will turn on green led.   
+            Serial.println("counterclockwise");
+            digitalWrite (RedLed, LOW);
+            digitalWrite (GreenLed, HIGH);
+        }
+        Serial.print("Encoder RotPosition: ");
+        Serial.println(RotPosition);
+    // this will print in the serial monitor.
+    } 
+    rotation = value;
+} 
